e94bcd5c8d3b72f752908c76b81b950848e08b2c
Mark Ross <rossma@berea.edu>
2015-07-22 15:53:59 -0400

    Fixed spacing and added a few php commands to clean up the html on loop-homepage-dept-3-col-posts.php

--- loop-homepage-dept-3-col-posts.php
+++ loop-homepage-dept-3-col-posts.php
@@ -17,17 +17,22 @@
  * @subpackage Starkers
  * @since Starkers 3.0
  */
+
+
+// If there are no posts to display,
+// such as an empty archive page
+if ( ! have_posts() ) :
+
 ?>
 
-<?php /* If there are no posts to display, such as an empty archive page */ ?>
-<?php if ( ! have_posts() ) : ?>
-		<h1><?php _e( 'Not Found', 'twentyten' ); ?></h1>
-		<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
-		<?php get_search_form(); ?>
+	<h1><?php _e( 'Not Found', 'twentyten' ); ?></h1>
+	<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyten' ); ?></p>
 
-<?php endif; ?>
+	<?php
+	get_search_form();
 
-<?php
+endif;
+
 	/* Start the Loop.
 	 *
 	 * In Twenty Ten we use the same loop in multiple contexts.
@@ -41,93 +46,156 @@
 	 * the rest of the loop that is shared.
 	 *
 	 * Without further ado, the loop:
-	 */ ?>
-<?php while ( have_posts() ) : the_post(); ?>
+	 */
+while ( have_posts() ) : the_post();
+	/* How to display posts in the Gallery category. */
+	if ( in_category( _x('gallery', 'gallery category slug', 'twentyten') ) ) : ?>
+		<h2><a href="<?php the_permalink(); ?>"
+			title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ),
+													 the_title_attribute( 'echo=0' ) ); ?>"
+			rel="bookmark">
+			<?php the_title(); ?>
+		</a></h2>
 
-<?php /* How to display posts in the Gallery category. */ ?>
+		<?php
+		twentyten_posted_on();
 
-	<?php if ( in_category( _x('gallery', 'gallery category slug', 'twentyten') ) ) : ?>
-			<h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<?php twentyten_posted_on(); ?>
+		if ( post_password_required() ) :
+			the_content();
 
-<?php if ( post_password_required() ) : ?>
-				<?php the_content(); ?>
-<?php else : ?>
-<?php
-	$images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC', 'numberposts' => 999 ) );
-	$total_images = count( $images );
-	$image = array_shift( $images );
-	$image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
-?>
-					<a href="<?php the_permalink(); ?>"><?php echo $image_img_tag; ?></a>
+		else :
+			$images = get_children( array( 'post_parent' => $post->ID,
+																		 'post_type' => 'attachment',
+																		 'post_mime_type' => 'image',
+																		 'orderby' => 'menu_order',
+																		 'order' => 'ASC',
+																		 'numberposts' => 999
+			) );
 
-				<p><?php printf( __( 'This gallery contains <a %1$s>%2$s photos</a>.', 'twentyten' ),
-						'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
-						$total_images
-					); ?></p>
+			$total_images = count( $images );
+			$image = array_shift( $images );
+			$image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
+		?>
+			<a href="<?php the_permalink(); ?>"><?php echo $image_img_tag; ?></a>
 
-				<?php the_excerpt(); ?>
-<?php endif; ?>
-
-				<a href="<?php echo get_term_link( _x('gallery', 'gallery category slug', 'twentyten'), 'category' ); ?>" title="<?php esc_attr_e( 'View posts in the Gallery category', 'twentyten' ); ?>"><?php _e( 'More Galleries', 'twentyten' ); ?></a>
-				|
-				<?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?>
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '|', '' ); ?>
-
-<?php /* How to display posts in the asides category */ ?>
-
-	<?php elseif ( in_category( _x('asides', 'asides category slug', 'twentyten') ) ) : ?>
-
-		<?php if ( is_archive() || is_search() ) : // Display excerpts for archives and search. ?>
-			<?php the_excerpt(); ?>
-		<?php else : ?>
-			<?php the_content( __( 'Continue reading &rarr;', 'twentyten' ) ); ?>
-		<?php endif; ?>
-
-				<?php twentyten_posted_on(); ?>
-				|
-				<?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?>
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '| ', '' ); ?>
-
-<?php /* How to display all other posts. */ ?>
-
-	<?php else : ?>
-			<h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<div style="color: #c75b12; padding-bottom:15px;"><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></div>
-
-	<?php if ( is_archive() || is_search() ) : // Only display excerpts for archives and search. ?>
-			<?php the_excerpt(); ?>
-	<?php else : ?>
-    		<?php global $more; $more = 0; ?>
-			<?php the_content( __( '<ul><li class="listchevron-green">Continue reading &rarr;</li></ul>', 'twentyten' ) ); ?>
-			<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
-	<?php endif; ?>
-				<div style="padding:5px; background-color:#E4E4E4; border:1px dashed #ccc;">
-				<?php if ( count( get_the_category() ) ) : ?>
-					<?php printf( __( 'Posted in %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
-					<span style="padding-left:8px;">|<span style="padding-left:8px;">
-				<?php endif; ?>
+			<p>
 				<?php
-					$tags_list = get_the_tag_list( '', ', ' );
-					if ( $tags_list ):
+				printf( __( 'This gallery contains <a %1$s>%2$s photos</a>.', 'twentyten' ),
+								'href="' . get_permalink() . '" title="' . sprintf( esc_attr__( 'Permalink to %s', 'twentyten' ),
+								 the_title_attribute( 'echo=0' ) ) . '" rel="bookmark"',
+								$total_images
+				);
 				?>
-					<?php printf( __( 'Tagged %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list ); ?>
+			</p>
+
+
+		<?php
+		the_excerpt();
+		endif;
+		?>
+
+	<a href="<?php echo get_term_link( _x('gallery', 'gallery category slug', 'twentyten'), 'category' ); ?>"
+		title="<?php esc_attr_e( 'View posts in the Gallery category', 'twentyten' ); ?>">
+		<?php _e( 'More Galleries', 'twentyten' ); ?>
+	</a>
 					|
-				<?php endif; ?>
-				<?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?>
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '| ', '' ); ?>
-                </div>
+	<?php
+	comments_popup_link( __( 'Leave a comment', 'twentyten' ),
+											 __( '1 Comment', 'twentyten' ),
+											 __( '% Comments', 'twentyten' )
+	);
+	edit_post_link( __( 'Edit', 'twentyten' ), '|', '' );
 
-		<?php comments_template( '', true ); ?>
+  /* How to display posts in the asides category */
+	elseif ( in_category( _x('asides', 'asides category slug', 'twentyten') ) ) :
 
-	<?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
+		if ( is_archive() || is_search() ) : // Display excerpts for archives and search.
+			the_excerpt();
+		else :
+			the_content( __( 'Continue reading &rarr;', 'twentyten' ) );
+		endif;
 
-<?php endwhile; // End the loop. Whew. ?>
+		twentyten_posted_on();
+		echo "|";
 
-<?php /* Display navigation to next/previous pages when applicable */ ?>
+		comments_popup_link( __( 'Leave a comment', 'twentyten' ),
+												 __( '1 Comment', 'twentyten' ),
+												 __( '% Comments', 'twentyten' )
+		);
+		edit_post_link( __( 'Edit', 'twentyten' ), '| ', '' );
+
+
+ /* How to display all other posts. */
+	else : ?>
+		<h2><a href="<?php the_permalink(); ?>"
+			title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ),
+													 the_title_attribute( 'echo=0' ) ); ?>"
+			rel="bookmark">
+			<?php the_title(); ?>
+		</a></h2>
+
+		<!-- NOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!  -->
+		<div style="color: #c75b12; padding-bottom:15px;">
+			<?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?>
+		</div>
+
+		<?php
+
+		// Only display excerpts for archives and search.
+		if ( is_archive() || is_search() ) :
+				the_excerpt();
+		else :
+	    	global $more;
+				$more = 0;
+				the_content( __( '<ul><li class="listchevron-green">Continue reading &rarr;</li></ul>', 'twentyten' ) );
+				wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) );
+		endif;
+
+		?>
+		<!-- NOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!  -->
+		<div style="padding:5px; background-color:#E4E4E4; border:1px dashed #ccc;">
+			<?php
+			if ( count( get_the_category() ) ) :
+				printf( __( 'Posted in %2$s', 'twentyten' ),
+								'entry-utility-prep entry-utility-prep-cat-links',
+								get_the_category_list( ', ' ) ); ?>
+
+				<!-- NOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!  -->
+				<span style="padding-left:8px;">|<span style="padding-left:8px;">
+
+			<?php
+			endif;
+
+			$tags_list = get_the_tag_list( '', ', ' );
+
+			if ( $tags_list ):
+				printf( __( 'Tagged %2$s', 'twentyten' ),
+								'entry-utility-prep entry-utility-prep-tag-links',
+								$tags_list );
+				echo "|";
+			endif;
+
+			comments_popup_link( __( 'Leave a comment', 'twentyten' ),
+													 __( '1 Comment', 'twentyten' ),
+													 __( '% Comments', 'twentyten' )
+			);
+			edit_post_link( __( 'Edit', 'twentyten' ), '| ', '' );
+			?>
+		</div>
+
+		<?php comments_template( '', true );
+
+	endif; // This was the if statement that broke the loop into three parts based on categories.
+endwhile; // End the loop. Whew.
+
+/* Display navigation to next/previous pages when applicable */
+?>
+<!-- NOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!  -->
 <div style="padding-top:15px;">
-<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-				<?php next_posts_link( __( '&larr; Older posts', 'twentyten' ) ); ?>
-				<?php previous_posts_link( __( 'Newer posts &rarr;', 'twentyten' ) ); ?>
-<?php endif; ?>
+<?php
+if (  $wp_query->max_num_pages > 1 ) :
+	next_posts_link( __( '&larr; Older posts', 'twentyten' ) );
+	previous_posts_link( __( 'Newer posts &rarr;', 'twentyten' ) );
+endif;
+?>
 </div>
