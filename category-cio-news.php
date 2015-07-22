aff610b65b22303e176f7a8822d48be9c1ba767c
Mark Ross <rossma@berea.edu>
2015-07-22 09:50:31 -0400

    Fixed spacing and line lengths in category-cio-news.php

--- category-cio-news.php
+++ category-cio-news.php
@@ -6,55 +6,54 @@
 get_header(); ?>
 
 <div id="middlepgbg">
-<div id="sectiontitle"><?php bloginfo( $show ); ?></div> 
-<div id="middle">
-	
-	<?php get_sidebar(); ?>
+	<div id="sectiontitle"><?php bloginfo( $show ); ?></div>
+	<div id="middle">
 
-<div id="col2">
-  <div id="col2col1">
-    <div id="content">
-		<?php
-			if (has_post_thumbnail()) {  
-			    the_post_thumbnail();  
-			}
-			?>
-			
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+		<?php get_sidebar(); ?>
 
-					<h1><?php the_title(); ?></h1>
+		<div id="col2">
+  		<div id="col2col1">
+    		<div id="content">
+					<?php
+					if (has_post_thumbnail())
+					  the_post_thumbnail();
+					?>
 
+					<?php //beginning of the loop
+					if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+						<h1><?php the_title(); ?></h1>
 						<p><?php twentyten_posted_on(); ?></p>
 
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
+						<?php
+						the_content();
+						wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ),
+																	'after' => '' ) );
+						if ( get_the_author_meta( 'description' ) ) :
+						// If a user has filled out their description,
+						// show a bio on their entries
+						endif;
 
-<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
-<?php endif; ?>
+						edit_post_link( __( 'Edit', 'twentyten' ), '', '' );
+						comments_template( '', true );
 
-						<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
+					endwhile; // end of the loop.
+					?>
+				</div>
+			</div>
 
+			<div id="col2col2">
+			  <?php // Begin Contact Info widget area
+				if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
+				 <div id="right-sidebar">
+				    <?php dynamic_sidebar( 'right-sidebar' ); ?>
+				 </div>
+				 <?php endif; // end Contact Info widget area ?>
+		 	</div>
 
-				<?php comments_template( '', true ); ?>
-
-<?php endwhile; // end of the loop. ?>
-</div>
-</div>
-
-<div id="col2col2">
-
-  <?php
-// Begin Contact Info widget area
-	if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
-	 <div id="right-sidebar">
-	    <?php dynamic_sidebar( 'right-sidebar' ); ?>
-	 </div>
- 
- <?php endif; 
-// end Contact Info widget area ?>
-</div>
-
-<div style="clear:both"></div>
-</div><div style="clear:both"></div></div>
+		 <div style="clear:both"></div>
+		</div>
+	<div style="clear:both"></div>
+	</div>
 </div>
 <?php get_footer(); ?>
