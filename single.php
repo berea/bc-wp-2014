de8e69dbefe534c27be446fd89d9d739d432d2bf
Mark Ross <rossma@berea.edu>
2015-07-22 16:58:51 -0400

    Added space and fixed the alignments.

--- single.php
+++ single.php
@@ -6,49 +6,52 @@
 get_header(); ?>
 
 <div id="middlepgbg">
-<div id="sectiontitle"><?php bloginfo( $show ); ?></div> 
-<div id="middle">
-	
+	<div id="sectiontitle"><?php bloginfo( $show ); ?></div>
+	<div id="middle">
+
 	<?php get_sidebar(); ?>
 
-<div id="col2">
-  <div id="col2col1">
-    <div id="content">
-		
-			
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+		<div id="col2">
+		  <div id="col2col1">
+		    <div id="content">
 
-					<h1><?php the_title(); ?></h1>
-				
-			            <p class="datetime"><?php twentyten_posted_on(); ?></p>
-						<?php the_content(); ?>
-						
-<?php if ( get_the_author_meta( 'description' ) ) : // If a user has filled out their description, show a bio on their entries  ?>
-<?php endif; ?>
+					<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+						<h1><?php the_title(); ?></h1>
+						<p class="datetime"><?php twentyten_posted_on(); ?></p>
+						<?php the_content();
 
-						<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
+						// If a user has filled out their description, show a bio on their entries
+						if ( get_the_author_meta( 'description' ) ) :
+						endif;
 
-<?php comments_template( '', true ); ?>
+						edit_post_link( __( 'Edit', 'twentyten' ), '', '' );
+						comments_template( '', true );
 
+					// end of the loop.
+					endwhile;
+					?>
+				</div>
+			</div>
 
-<?php endwhile; // end of the loop. ?>
+			<div id="col2col2">
+			  <?php
+				// Begin Contact Info widget area
+				if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
+				 <div id="right-sidebar">
+				    <?php dynamic_sidebar( 'right-sidebar' ); ?>
+				 </div>
+
+			 <?php endif;
+			// end Contact Info widget area ?>
+			</div>
+
+		<!-- NOOOOOOOOOOOOOOOOO!!!! -->
+		<div style="clear:both"></div>
+		</div>
+	<!-- NOOOOOOOOOOOOOOOOO!!!! -->
+	<div style="clear:both"></div>
+	</div>
+
 </div>
-</div>
 
-<div id="col2col2">
-
-  <?php
-// Begin Contact Info widget area
-	if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
-	 <div id="right-sidebar">
-	    <?php dynamic_sidebar( 'right-sidebar' ); ?>
-	 </div>
- 
- <?php endif; 
-// end Contact Info widget area ?>
-</div>
-
-<div style="clear:both"></div>
-</div><div style="clear:both"></div></div>
-</div>
 <?php get_footer(); ?>
