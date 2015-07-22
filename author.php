c7fdaec2f2ae16607f475aefdef98f434667067e
Mark Ross <rossma@berea.edu>
2015-07-22 09:37:58 -0400

    Fixed tab spacing in author.php

--- author.php
+++ author.php
@@ -15,51 +15,47 @@ get_header(); ?>
 	if ( have_posts() )
 		the_post();
 ?>
+
 <div id="middlepgbg">
-<div id="sectiontitle"><?php bloginfo( $show ); ?></div> 
-<div id="middle">
-	
-	<?php get_sidebar(); ?>
+	<div id="sectiontitle"><?php bloginfo( $show ); ?></div>
+	<div id="middle">
 
-<div id="col2">
-  
-  <div id="col2col1">
-    
-    <div id="content">
-	
-		<h1>Recent Posts from <?php printf( '%s', get_the_author() ); ?></h1>
-			
+		<?php get_sidebar(); ?>
 
-<?php
-	/* Since we called the_post() above, we need to
-	 * rewind the loop back to the beginning that way
-	 * we can run the loop properly, in full.
-	 */
-	rewind_posts();
+		<div id="col2">
+		  <div id="col2col1">
+		    <div id="content">
 
-	/* Run the loop for the author archive page to output the authors posts
-	 * If you want to overload this in a child theme then include a file
-	 * called loop-author.php and that will be used instead.
-	 */
-	 get_template_part( 'loop', 'author' );
-?>
-      </div>
-</div>
+					<h1>Recent Posts from <?php printf( '%s', get_the_author() ); ?></h1>
 
-<div id="col2col2">
-	
-	  <?php
-	// Begin Contact Info widget area
-		if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
-		 <div id="right-sidebar">
-		    <?php dynamic_sidebar( 'right-sidebar' ); ?>
-		 </div>
-	 
-	 <?php endif; 
-	// end Contact Info widget area ?>
-</div>
+					<?php
+						/* Since we called the_post() above, we need to
+						 * rewind the loop back to the beginning that way
+						 * we can run the loop properly, in full.
+						 */
+						rewind_posts();
 
-<div style="clear:both"></div>
-</div><div style="clear:both"></div></div>
+						/* Run the loop for the author archive page to output the authors posts
+						 * If you want to overload this in a child theme then include a file
+						 * called loop-author.php and that will be used instead.
+						 */
+						 get_template_part( 'loop', 'author' );
+					?>
+	      </div>
+			</div>
+
+			<div id="col2col2">
+		  	<?php // Begin Contact Info widget area
+				if ( is_active_sidebar( 'right-sidebar' ) ) : ?>
+					 <div id="right-sidebar">
+					    <?php dynamic_sidebar( 'right-sidebar' ); ?>
+					 </div>
+		 	 <?php endif; // end Contact Info widget area ?>
+		 	</div>
+
+			<div style="clear:both"></div>
+		</div>
+		<div style="clear:both"></div>
+	</div>
 </div>
 <?php get_footer(); ?>
