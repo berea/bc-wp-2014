7e4957e5103c5cb0c5e981115d7820693e221bd0
Dan Adams <daniel_adams@berea.edu>
2017-02-03 11:08:56 -0500

    removing GA

--- functions.php
+++ functions.php
@@ -15,23 +15,6 @@ function load_custom_widgets()
 
 // END Load custom-widgets.php file
 
-// BEGIN Google Analytics Code Insertion
-// -- added by Cary W. Hazelwood
-// -- updated July 20, 2015 @1:45pm by Charlie Campbell
-add_action('wp_footer', 'add_googleanalytics');
-function add_googleanalytics() { ?>
-    <!-- Google Analytics BEGIN -->
-    <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-22618401-1', 'auto');
-    ga('send', 'pageview');
-    </script>
-    <!-- Google Analytics END -->
-<?php }
-// END Google Analytics Code Insertion
 
 // BEGIN Pingdom and Hotjar Code Insertion
 // -- added by Charlie C Campbell
